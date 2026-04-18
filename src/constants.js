bd224cb1cf321977550f1b75d5a99beac3a8ad54
sfaber34 <sfaber34@gmail.com>
2026-04-17 21:10:29 -0600

    messing with death sfx

--- src/constants.js
+++ src/constants.js
@@ -287,7 +287,7 @@ export const DEATH_SFX_VOLUME = 0.45; // max volume at point blank
 export const DEATH_SFX_MIN_VOLUME_FRAC = 0.45; // floor volume fraction at max distance (prevents silent deaths)
 export const DEATH_SFX_MAX_CONCURRENT = 3; // max death sounds playing at once (extras dropped)
 export const DEATH_SFX_STAGGER_MS = 150; // ms delay between staggered death sounds in a mass kill
-export const DEATH_SFX_COOLDOWN = 3; // how many other death sounds must play before one can repeat
+export const DEATH_SFX_COOLDOWN = 10; // how many other death sounds must play before one can repeat
 
 // --- Engine SFX (looping, pitch/volume shift with drone speed) ---
 export const ENGINE_VOLUME_MIN = 0.04; // volume at minimum speed
